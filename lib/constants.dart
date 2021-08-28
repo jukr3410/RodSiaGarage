c50d1992bfb5fffe9ec83e2383d8ea57c4a1b1aa
pimpcnn <56501828+jukr3410@users.noreply.github.com>
2021-08-28 22:58:30 +0700

    [add] constants

---
+++ lib/constants.dart
@@ -0,0 +1,43 @@
+//api base url
+//dev localhost api
+import 'package:flutter/material.dart';
+
+final baseUrlConstant = "http://localhost:3000/api";
+
+// production api
+//final baseUrlConstant = '';
+
+// color that use in app
+const textColorBlack = Color(0xff38454C);
+const textColorWhite = Colors.white;
+const textColorRed = Color(0xffDA5B49);
+
+const primaryColor = Color(0xffFECE2F);
+const colorGray = Color(0xffC4C4C4);
+const bgColor = Color(0xffF2F4F6);
+
+const cardColor = Colors.white;
+
+// message, wording
+const mError = "Service unvalidable, please try agian.";
+const mLoading = "loading";
+const mNotFound = "not found!";
+
+// src string for set widget
+const tOKThai = 'ตกลง';
+const tServiceOfGarage = 'บริการของร้าน';
+const tAlertRequestService = 'มีคนต้องการความช่วยเหลือ!';
+const tRequestServiceThai = 'ขอรับบริการ';
+const tDeclineThai = 'ปฏิเสธิ';
+const tAddressThai = 'ที่อยู่: ';
+const tUpdateThai = 'อัพเดต';
+const tCancleThai = 'ยกเลิก';
+const tAcceptThai = 'ยอมรับ';
+const tYesThai = 'ใช้';
+const tMessage = "messages";
+const tEmail = "E-mail";
+const tPassword = "Password";
+const tNameGarageThai = "ชื่ออู๋:";
+const tNameUserThai = "ชื่อลูกค้า:";
+const tLocationThai = 'ตำแหน่ง:';
+const tProblemThai = 'ปัญหา:';
---
+++ lib/core/repository/garage_api.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+import 'package:rodsiagarage/constants.dart';
+import 'package:rodsiagarage/core/models/garage_model.dart';
+import 'package:http/http.dart' as http;
+
+class GarageApi {
+  final baseUrl = baseUrlConstant;
+
+  //late final http.Client httpClient;
+
+  //GarageApi({required this.httpClient});
+
+  Future<Garage> getGarageInfo({required String id}) async {
+    final url = '$baseUrl/garage-id/$id';
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode != 200) {
+      throw new Exception('There was a problem ${response.statusCode}');
+    }
+    final decodedJson = jsonDecode(response.body);
+    Garage garage = decodedJson;
+
+    return garage;
+  }
+}
