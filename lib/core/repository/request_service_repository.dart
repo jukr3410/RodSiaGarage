3983530cccb532d4a20fbe641e9f39c9f853efd7
pimpcnn <56501828+jukr3410@users.noreply.github.com>
2021-09-14 23:09:52 +0700

    Update request_service_repository.dart

--- lib/core/repository/request_service_repository.dart
+++ lib/core/repository/request_service_repository.dart
@@ -1,3 +1,4 @@
+import 'package:rodsiagarage/core/models/geo_location_model.dart';
 import 'package:rodsiagarage/core/models/request_service_model.dart';
 import 'package:rodsiagarage/core/repository/request_service_api.dart';
 
@@ -14,5 +15,17 @@ class RequestServiceRepository {
 
   Future<RequestService> getRequestService({required String id}) async {
     return await requestServiceApi.getRequestService(id: id);
+  }
+
+  Future<bool> updateRequestStatus(
+      {required String requestId, required String status}) async {
+    return await requestServiceApi.updateRequestStatus(
+        requestId: requestId, status: status);
+  }
+
+  Future<bool> updateGeoLocation(
+      {required String requestId, required GeoLocation geoLocation}) async {
+    return await requestServiceApi.updateGeoLocation(
+        requestId: requestId, geoLocation: geoLocation);
   }
 }
