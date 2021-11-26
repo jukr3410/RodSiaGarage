cf03121646945126164d8cd26a101bc338fe78a0
pimpcnn <56501828+jukr3410@users.noreply.github.com>
2021-11-26 18:27:30 +0700

    [update] recap req service

--- lib/request_service_feature/widgets/requestDetailAndGiveStarPage.dart
+++ lib/request_service_feature/widgets/requestDetailAndGiveStarPage.dart
@@ -35,90 +35,106 @@ class _DetailAndGiveStarPageState extends State<DetailAndGiveStarPage> {
           ),
         ),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(defualtPaddingLow),
-          child: Form(
-            child: Column(
-              children: [
-                CircleAvatar(
-                  backgroundColor: Colors.transparent,
-                  radius: 60,
-                  child: ClipOval(
-                    child: Image.asset(
-                      tImageAsset('profile-homePage'),
-                      height: 100,
-                      width: 100,
-                      fit: BoxFit.cover,
+      body: Container(
+        color: textColorWhite,
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+              child: Column(
+                children: [
+                  CircleAvatar(
+                    backgroundColor: Colors.transparent,
+                    radius: 60,
+                    child: ClipOval(
+                      child: Image.asset(
+                        tImageAsset('profile-homePage'),
+                        height: 100,
+                        width: 100,
+                        fit: BoxFit.cover,
+                      ),
                     ),
                   ),
-                ),
-                Text(
-                  'Jonh Cena',
-                  style: TextStyle(fontSize: fontSizeXXl),
-                ),
-                SizedBox(height: 40),
-                Align(
-                    alignment: Alignment.centerLeft,
+                  Text(
+                    widget.requestService.user.name,
+                    style: TextStyle(fontSize: fontSizeXXl),
+                  ),
+                  SizedBox(height: 20),
+                  Divider(
+                    color: Colors.black,
+                  ),
+                  SizedBox(height: 20),
+                  Align(
+                      alignment: Alignment.centerLeft,
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 0),
+                        child: Row(
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'บริการที่ใช้:',
+                                  style: TextStyle(
+                                      fontSize: fontSizeXl,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                SizedBox(height: 5),
+                                Text(
+                                  widget
+                                      .requestService.service.serviceType.name,
+                                ),
+                                SizedBox(height: 5),
+                                Text(widget.requestService.service.name),
+                                SizedBox(height: 5),
+                                Text(
+                                  tMoreInfoThai,
+                                  softWrap: true,
+                                  maxLines: 3,
+                                ),
+                                SizedBox(height: 5),
+                                Text('เวลา'),
+                                SizedBox(height: 20),
+                                Text(
+                                  'สถานที่: ',
+                                  style: TextStyle(
+                                      fontSize: fontSizeXl,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                SizedBox(height: 5),
+                                Text(
+                                  widget.requestService.addressUser,
+                                  softWrap: true,
+                                  maxLines: 3,
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                      )),
+                  SizedBox(height: 20),
+                  Expanded(
                     child: Padding(
-                      padding:
-                          const EdgeInsets.only(left: defualtPaddingMedium),
-                      child: Row(
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'บริการที่ใช้:',
-                                style: TextStyle(
-                                    fontSize: fontSizeXl,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                              Text(
-                                'Service type',
-                              ),
-                              Text('Service'),
-                              Text(
-                                tMoreInfoThai,
-                                softWrap: true,
-                                maxLines: 3,
-                              ),
-                              Text('เวลา'),
-                              SizedBox(height: 20),
-                              Text(
-                                'สถานที่: ',
-                                style: TextStyle(
-                                    fontSize: fontSizeXl,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                              Text(
-                                '23456 แขวงนู่น เขตนี่',
-                                softWrap: true,
-                                maxLines: 3,
-                              )
-                            ],
+                      padding: const EdgeInsets.only(bottom: 16.0),
+                      child: Align(
+                        alignment: FractionalOffset.bottomCenter,
+                        child: Container(
+                          height: buttonHeightSmall + 5,
+                          width: buttonWidthLarge,
+                          child: TextButton(
+                            onPressed: () {
+                              navigateToHome();
+                            },
+                            child: Text(tBackToHomePage),
+                            style:
+                                flatButtonStyle(primaryColor, textColorBlack),
                           ),
-                        ],
-                      ),
-                    )),
-                SizedBox(height: 20),
-                Expanded(
-                  child: Align(
-                    alignment: FractionalOffset.bottomCenter,
-                    child: Container(
-                      height: buttonHeightSmall + 5,
-                      width: buttonWidthLarge,
-                      child: TextButton(
-                        onPressed: () {
-                          navigateToHome();
-                        },
-                        child: Text(tBackToHomePage),
-                        style: flatButtonStyle(primaryColor, textColorBlack),
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
